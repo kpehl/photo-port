75bc654d84307826c16063784bd5437edbdc3ab3
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-19 17:39:11 -0500

    added testing to About component and verified rendering and matched snapshot via test suite

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // renders About test
+    // First Test - verify that the component is rendering
+    it('renders', () => {
+        render(<About />);
+    });
+    // Second Test - compare snapshot versions of the DOM node structure
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
