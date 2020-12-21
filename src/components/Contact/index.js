6ff009530be83dfdcab96b9830fc8c7ea668b387
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-12-21 10:51:30 -0500

    added contact form to App.js and created functions to log the user input

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,31 @@
 // Contact Form Component
-import React from 'react';
+import React, {useState} from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+    function handleChange(event) {
+        setFormState({...formState, [event.target.name]: event.target.value })
+    }
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState)
+    }
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
